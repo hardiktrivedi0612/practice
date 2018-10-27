fd36202b45bbe2ae1ca06e5cc4621d0627ea52bc
Hardik Trivedi <hardiktrivedi0612@gmail.com>
2018-10-27 12:57:02 -0500

    BinaryTreeZigZagLevelOrderTraversal

---
+++ src/main/java/com/trivedi/hardik/leetcode/TreeNode.java
@@ -0,0 +1,16 @@
+package com.trivedi.hardik.leetcode;
+
+/**
+ * @author hatrivedi
+ * @date Oct 27, 2018
+ * @since 2.5
+ */
+public class TreeNode {
+	public int val;
+	public TreeNode left;
+	public TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
---
+++ src/main/java/com/trivedi/hardik/leetcode/bfs/BinaryTreeZigZagLevelOrderTraversal.java
@@ -0,0 +1,38 @@
+package com.trivedi.hardik.leetcode.bfs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.trivedi.hardik.leetcode.TreeNode;
+
+/**
+ * @author hatrivedi
+ * @date Oct 27, 2018
+ * @since 2.5
+ */
+public class BinaryTreeZigZagLevelOrderTraversal {
+	public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+		List<List<Integer>> response = new ArrayList<>();
+		dfsTraversal(root, 0, response);
+		return response;
+	}
+
+	public void dfsTraversal(TreeNode root, int depth, List<List<Integer>> response) {
+		if (root == null)
+			return;
+		if (response.size() - 1 < depth) {
+			response.add(new ArrayList<Integer>());
+		}
+
+		if (depth % 2 == 0) {
+			response.get(depth).add(root.val);
+		} else {
+			response.get(depth).add(0, root.val);
+		}
+
+		dfsTraversal(root.left, depth + 1, response);
+		dfsTraversal(root.right, depth + 1, response);
+
+		return;
+	}
+}
