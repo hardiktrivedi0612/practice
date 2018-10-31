389bd9772d7b096d7ff431affd745823bcb0ab5d
Hardik Trivedi <hardiktrivedi0612@gmail.com>
2018-10-31 15:53:37 -0500

    Minimum depth of binary tree

---
+++ src/main/java/com/trivedi/hardik/leetcode/arrays/SortColors.java
@@ -0,0 +1,34 @@
+package com.trivedi.hardik.leetcode.arrays;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * @author hatrivedi
+ * @date Oct 27, 2018
+ * @since 2.5
+ */
+public class SortColors {
+	
+	//My Solution
+	public void sortColors(int[] nums) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for(int i : nums) {
+            if(!map.containsKey(i)) {
+                map.put(i, 1);
+            } else {
+                map.put(i, map.get(i) + 1);
+            }
+        }
+        int i = 0;
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            for(int j = 0; j < entry.getValue(); j++) {
+                nums[i++] = entry.getKey();
+            }
+        }
+    }
+	
+	
+	//Optimized solution
+	
+}
---
+++ src/main/java/com/trivedi/hardik/leetcode/dfs/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,18 @@
+package com.trivedi.hardik.leetcode.dfs;
+
+import com.trivedi.hardik.leetcode.TreeNode;
+
+/**
+ * @author hatrivedi
+ * @date Oct 31, 2018
+ * @since 2.5
+ */
+public class MinimumDepthOfBinaryTree {
+	public int minDepth(TreeNode root) {
+		if (root == null)
+			return 0;
+		int leftDepth = minDepth(root.left);
+		int rightDepth = minDepth(root.right);
+		return (leftDepth == 0 || rightDepth == 0) ? leftDepth + rightDepth + 1 : Math.min(leftDepth, rightDepth) + 1;
+	}
+}
