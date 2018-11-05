9a2f485ade10034b344de3ccfdc9a8539569aa49
Hardik Trivedi <hardiktrivedi0612@gmail.com>
2018-11-05 16:30:57 -0600

    Number of island - Started

--- src/main/java/com/trivedi/hardik/leetcode/SplitBST.java
+++ src/main/java/com/trivedi/hardik/leetcode/SplitBST.java
@@ -7,13 +7,3 @@ public class SplitBST {
 	}
 
 }
-
-class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
-
-	TreeNode(int x) {
-		val = x;
-	}
-}
---
+++ src/main/java/com/trivedi/hardik/leetcode/dfs/NumberOfIslands.java
@@ -0,0 +1,21 @@
+package com.trivedi.hardik.leetcode.dfs;
+
+/**
+ * @author hatrivedi
+ * @date Nov 5, 2018
+ * @since 2.5
+ */
+public class NumberOfIslands {
+	public int numIslands(char[][] grid) {
+		
+		int m = grid.length;
+		int n = grid[0].length;
+		
+		if(m == 0 || n == 0) {
+			return 0;
+		}
+		
+		
+		
+	}
+}
