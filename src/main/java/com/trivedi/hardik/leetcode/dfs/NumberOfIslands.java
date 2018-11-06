5075f2ce763587e21454b6409679d7811009a233
Hardik Trivedi <hardiktrivedi0612@gmail.com>
2018-11-06 10:21:07 -0600

    Number of island

--- src/main/java/com/trivedi/hardik/leetcode/dfs/NumberOfIslands.java
+++ src/main/java/com/trivedi/hardik/leetcode/dfs/NumberOfIslands.java
@@ -7,15 +7,34 @@ package com.trivedi.hardik.leetcode.dfs;
  */
 public class NumberOfIslands {
 	public int numIslands(char[][] grid) {
-		
-		int m = grid.length;
-		int n = grid[0].length;
-		
-		if(m == 0 || n == 0) {
+
+		if (grid.length == 0 || grid[0].length == 0) {
 			return 0;
 		}
-		
-		
-		
+
+		int count = 0;
+
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				if (grid[i][j] == '1') {
+					count++;
+					performDFSAndClearIsland(grid, i, j);
+				}
+			}
+		}
+
+		return count;
+
+	}
+
+	private void performDFSAndClearIsland(char[][] grid, int i, int j) {
+		if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0') {
+			return;
+		}
+		grid[i][j] = '0';
+		performDFSAndClearIsland(grid, i, j - 1);
+		performDFSAndClearIsland(grid, i, j + 1);
+		performDFSAndClearIsland(grid, i + 1, j);
+		performDFSAndClearIsland(grid, i - 1, j);
 	}
 }
