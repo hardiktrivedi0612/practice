619f5352424250bce60e5967af9e037d64fc1dca
Hardik Trivedi <hardiktrivedi0612@gmail.com>
2018-11-30 13:31:40 -0600

    Copy list with random pointer

--- src/main/java/com/trivedi/hardik/leetcode/CopyListWithRandomPointer.java
+++ src/main/java/com/trivedi/hardik/leetcode/CopyListWithRandomPointer.java
@@ -1,5 +1,8 @@
 package com.trivedi.hardik.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author hatrivedi
  * @date Nov 6, 2018
@@ -7,6 +10,21 @@ package com.trivedi.hardik.leetcode;
  */
 public class CopyListWithRandomPointer {
 	public RandomListNode copyRandomList(RandomListNode head) {
-
+		if (head == null) {
+			return null;
+		}
+		Map<RandomListNode, RandomListNode> map = new HashMap<>();
+		RandomListNode pointer = head;
+		while (pointer != null) {
+			map.put(pointer, new RandomListNode(pointer.label));
+			pointer = pointer.next;
+		}
+		pointer = head;
+		while (pointer != null) {
+			map.get(pointer).next = map.get(pointer.next);
+			map.get(pointer).random = map.get(pointer.random);
+			pointer = pointer.next;
+		}
+		return map.get(head);
 	}
 }
