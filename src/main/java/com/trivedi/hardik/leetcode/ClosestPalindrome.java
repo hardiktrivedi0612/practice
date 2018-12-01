d178c42f717644dcfae28d8b7a962d504f967121
Hardik Trivedi <hardiktrivedi0612@gmail.com>
2018-12-01 12:39:57 -0600

    Started nearest palindrome

---
+++ src/main/java/com/trivedi/hardik/leetcode/ClosestPalindrome.java
@@ -0,0 +1,12 @@
+package com.trivedi.hardik.leetcode;
+
+/**
+ * @author hatrivedi
+ * @date Dec 1, 2018
+ * @since 2.5
+ */
+public class ClosestPalindrome {
+	public String nearestPalindromic(String n) {
+		
+	}
+}
