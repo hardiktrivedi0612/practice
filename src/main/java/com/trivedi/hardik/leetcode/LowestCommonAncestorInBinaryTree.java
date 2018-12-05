83c326058121f8dcc0f4d8401ee5e40b8725de18
Hardik Trivedi <hardiktrivedi0612@gmail.com>
2018-12-04 19:58:46 -0600

    Common ancestor in binary tree

---
+++ src/main/java/com/trivedi/hardik/leetcode/LowestCommonAncestorInBinaryTree.java
@@ -0,0 +1,21 @@
+package com.trivedi.hardik.leetcode;
+
+/**
+ * @author hatrivedi
+ * @date Dec 4, 2018
+ * @since 2.5
+ */
+public class LowestCommonAncestorInBinaryTree {
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		if (root == null || root == p || root == q) {
+			return root;
+		}
+		TreeNode leftAncestor = lowestCommonAncestor(root.left, p, q);
+		TreeNode rightAncestor = lowestCommonAncestor(root.right, p, q);
+		if (leftAncestor != null && rightAncestor != null) {
+			// Ancestor is found
+			return root;
+		}
+		return (leftAncestor == null) ? rightAncestor : leftAncestor;
+	}
+}
