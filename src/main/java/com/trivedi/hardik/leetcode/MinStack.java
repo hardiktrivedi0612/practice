80a335dc9d662cecba8deca11322dc34ff1ecb24
Hardik Trivedi <hardiktrivedi0612@gmail.com>
2018-12-04 20:39:06 -0600

    Minimum stack

---
+++ src/main/java/com/trivedi/hardik/leetcode/MinStack.java
@@ -0,0 +1,49 @@
+package com.trivedi.hardik.leetcode;
+
+import java.util.Stack;
+
+/**
+ * Design a stack that supports push, pop, top, and retrieving the minimum
+ * element in constant time.
+ * 
+ * push(x) -- Push element x onto stack. pop() -- Removes the element on top of
+ * the stack. top() -- Get the top element. getMin() -- Retrieve the minimum
+ * element in the stack.
+ * 
+ * @author hatrivedi
+ * @date Dec 4, 2018
+ * @since 2.5
+ */
+public class MinStack {
+
+	Stack<Integer> mainStack;
+	Stack<Integer> minStack;
+
+	/** initialize your data structure here. */
+	public MinStack() {
+		mainStack = new Stack<>();
+		minStack = new Stack<>();
+	}
+
+	public void push(int x) {
+		mainStack.push(x);
+		if (minStack.isEmpty() || minStack.peek() >= x) {
+			minStack.push(x);
+		}
+	}
+
+	public void pop() {
+		Integer poppedValue = mainStack.pop();
+		if (poppedValue == minStack.peek()) {
+			minStack.pop();
+		}
+	}
+
+	public int top() {
+		return mainStack.peek();
+	}
+
+	public int getMin() {
+		return minStack.peek();
+	}
+}
