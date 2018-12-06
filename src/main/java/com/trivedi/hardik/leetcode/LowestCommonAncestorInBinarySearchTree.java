cee07d472b1b19cbdba44bda483c0f8e255a7b83
Hardik Trivedi <hardiktrivedi0612@gmail.com>
2018-12-05 22:59:35 -0600

    Lowest common ancestor in bst

--- src/main/java/com/trivedi/hardik/leetcode/LowestCommonAncestorInBinarySearchTree.java
+++ src/main/java/com/trivedi/hardik/leetcode/LowestCommonAncestorInBinarySearchTree.java
@@ -13,5 +13,12 @@ package com.trivedi.hardik.leetcode;
  * @since 2.5
  */
 public class LowestCommonAncestorInBinarySearchTree {
-
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		if (p.val > root.val && q.val > root.val) {
+			return lowestCommonAncestor(root.right, p, q);
+		} else if (p.val < root.val && q.val < root.val) {
+			return lowestCommonAncestor(root.left, p, q);
+		}
+		return root;
+	}
 }
