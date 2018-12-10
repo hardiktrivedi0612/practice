0383a8865dabde9b53a1992bf329fbb2d00148a5
Hardik Trivedi <hardiktrivedi0612@gmail.com>
2018-12-10 15:53:47 -0600

    Bottom left tree value

--- src/main/java/com/trivedi/hardik/leetcode/BottomLeftTreeValue.java
+++ src/main/java/com/trivedi/hardik/leetcode/BottomLeftTreeValue.java
@@ -1,5 +1,8 @@
 package com.trivedi.hardik.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Given a binary tree, find the leftmost value in the last row of the tree.
  * 
@@ -9,6 +12,20 @@ package com.trivedi.hardik.leetcode;
  */
 public class BottomLeftTreeValue {
 	public int findBottomLeftValue(TreeNode root) {
+		List<List<Integer>> response = new ArrayList<>();
+		dfs(root, response, 0);
+		return response.get(response.size() - 1).get(0);
+	}
 
+	public void dfs(TreeNode root, List<List<Integer>> response, int depth) {
+		if (root == null) {
+			return;
+		}
+		if (response.size() - 1 < depth) {
+			response.add(new ArrayList<Integer>());
+		}
+		dfs(root.left, response, depth + 1);
+		response.get(depth).add(root.val);
+		dfs(root.right, response, depth + 1);
 	}
 }
